9a6e9470e56a9497824ecdc07a2cb62287b40109
anis <anisoncloud@gmail.com>
2019-10-07 11:49:06 +0600

    Initial Commit

---
+++ DAL/UniversityContext.cs
@@ -0,0 +1,21 @@
+﻿using DhakaUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DhakaUniversity.DAL
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
